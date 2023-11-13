905cf209aee6d6d0444b13f2a59fac92b39a6e16
Dave Pearson <davep@davep.org>
2023-11-13 18:12:42 +0000

    :sparkles: Add functions for getting the location of the bookmark file

---
+++ tinboard/utils/__init__.py
@@ -0,0 +1,11 @@
+"""Utility code for the application."""
+
+##############################################################################
+# Local imports.
+from .data import bookmarks_file, data_dir
+
+##############################################################################
+# Exports.
+__all__ = ["bookmarks_file", "data_dir"]
+
+### __init__.py ends here
---
+++ tinboard/utils/data.py
@@ -0,0 +1,37 @@
+"""Utility code for dealing with the data."""
+
+##############################################################################
+# Python imports.
+from pathlib import Path
+
+##############################################################################
+# XDG imports.
+from xdg_base_dirs import xdg_data_home
+
+
+##############################################################################
+def data_dir() -> Path:
+    """The path to the data directory for the application.
+
+    Returns:
+        The path to the data directory for the application.
+
+    Note:
+        If the directory doesn't exist, it will be created as a side-effect
+        of calling this functon.
+    """
+    (save_to := xdg_data_home() / "tinboard").mkdir(parents=True, exist_ok=True)
+    return save_to
+
+
+##############################################################################
+def bookmarks_file() -> Path:
+    """The path to the file that the local bookmarks are held in.
+
+    Returns:
+        The path to the bookmarks file.
+    """
+    return data_dir() / "bookmarks.json"
+
+
+### data.py ends here
