daf5e82cb0aafecf1c12368b0528c41851ba7e43
Dave Pearson <davep@davep.org>
2023-11-15 23:01:03 +0000

    :construction: Revamping the Details pane
    Mostly there with how I want it to look, but I need to spend a bit of time
    thinking about how I'm going to do the tags. Using links in Markdown is
    great and all, but it's a bit keyboard-hostile. I want to be
    keyboard-friendly too.
    
    So this is a WiP commit at the end of the evening, and the next time I'm in
    here I'll have a proper think about how I'm going to do this.
    
    See also https://github.com/Textualize/textual/issues/3687 as a related
    issue that I've discovered while making these changes (it explains why the
    scrollbar in the details pane isn't showing at the moment, if it's needed).

--- tinboard/widgets/details.py
+++ tinboard/widgets/details.py
@@ -6,6 +6,10 @@ from dataclasses import dataclass
 from webbrowser import open as open_url
 
 ##############################################################################
+# Humanize imports.
+from humanize import naturaltime
+
+##############################################################################
 # Textual imports.
 from textual import on
 from textual.app import ComposeResult
@@ -13,7 +17,7 @@ from textual.binding import Binding
 from textual.containers import VerticalScroll
 from textual.message import Message
 from textual.reactive import var
-from textual.widgets import Markdown
+from textual.widgets import Label
 
 ##############################################################################
 # Local imports.
@@ -24,6 +28,50 @@ from .bookmarks import Bookmark
 class Details(VerticalScroll):
     """A widget for displaying details of a bookmark."""
 
+    DEFAULT_CSS = """
+    Details {
+        scrollbar-gutter: stable;
+    }
+
+    Details .hidden {
+        visibility: hidden;
+    }
+
+    Details .empty {
+        display: none;
+    }
+
+    Details Label {
+        margin: 0 2 1 2;
+        width: 1fr;
+        color: $text;
+    }
+
+    Details #title {
+        background: $primary;
+        padding: 1 2 1 2;
+        text-align: center;
+    }
+
+    Details .detail {
+        background: $boost;
+        padding: 1 2 1 2;
+    }
+
+    Details #last-modified-ish {
+        margin: 0 2 0 2;
+        padding: 1 2 0 2;
+    }
+
+    Details #last-modified-exact {
+        margin: 0 2 1 2;
+        padding: 0 2 1 2;
+        text-align: right;
+        color: $text-muted;
+        text-style: italic;
+    }
+    """
+
     BINDINGS = [Binding("enter", "visit_bookmark", "Visit")]
 
     bookmark: var[Bookmark | None] = var(None)
@@ -31,7 +79,14 @@ class Details(VerticalScroll):
 
     def compose(self) -> ComposeResult:
         """Compose the widget."""
-        yield Markdown()
+        yield Label(id="title")
+        yield Label(id="description", classes="detail")
+        yield Label(id="link", classes="detail")
+        yield Label(id="last-modified-ish", classes="detail")
+        yield Label(id="last-modified-exact", classes="detail")
+        yield Label(id="is-read", classes="detail")
+        yield Label(id="is-public", classes="detail")
+        yield Label("TODO: Tags")
 
     @property
     def _tags(self) -> str:
@@ -47,19 +102,32 @@ class Details(VerticalScroll):
 
     def _watch_bookmark(self) -> None:
         """React to the bookmark being changed."""
-        self.query_one(Markdown).update(
-            ""
-            if self.bookmark is None
-            else (
-                f"# {self.bookmark.title}\n"
-                f"{self.bookmark.description}\n"
-                f"## Link\n[{self.bookmark.href}]({self.bookmark.href})\n"
-                f"## Last Modified\n{self.bookmark.last_modified}\n"
-                f"## Tags\n{self._tags}\n"
-                f"## Read\n{not self.bookmark.unread}\n"
-                f"## Public\n{self.bookmark.shared}\n"
+        try:
+            if self.bookmark is None:
+                return
+            self.query_one("#title", Label).update(self.bookmark.title)
+            self.query_one("#description", Label).update(self.bookmark.description)
+            self.query_one("#description", Label).set_class(
+                not bool(self.bookmark.description), "empty"
             )
-        )
+            # TODO: This doesn't find the correct action.
+            self.query_one("#link", Label).update(
+                f"[@click=visit_bookmark]{self.bookmark.href}[/]"
+            )
+            self.query_one("#last-modified-ish", Label).update(
+                f"Last updated {naturaltime(self.bookmark.last_modified)}"
+            )
+            self.query_one("#last-modified-exact", Label).update(
+                str(self.bookmark.last_modified)
+            )
+            self.query_one("#is-read", Label).update(
+                f"The bookmark has {'[i]not[/] ' if self.bookmark.unread else ''}been read"
+            )
+            self.query_one("#is-public", Label).update(
+                f"The bookmark is {'[bold]public[/]' if self.bookmark.shared else '[dim]private[/]'}"
+            )
+        finally:
+            self.query("*").set_class(not bool(self.bookmark), "hidden")
 
     def action_visit_bookmark(self) -> None:
         """Visit the current bookmark, if there is one."""
@@ -73,18 +141,18 @@ class Details(VerticalScroll):
         tag: str
         """The tag to filter the bookmarks with."""
 
-    @on(Markdown.LinkClicked)
-    def visit_link(self, event: Markdown.LinkClicked) -> None:
-        """Visit any link clicked on the markdown.
+    # @on(Markdown.LinkClicked)
+    # def visit_link(self, event: Markdown.LinkClicked) -> None:
+    #     """Visit any link clicked on the markdown.
 
-        Args:
-            event: The click event.
-        """
-        if event.href.startswith("tag:"):
-            *_, tag = event.href.partition("tag:")
-            self.post_message(self.ShowTaggedWith(tag))
-        else:
-            open_url(event.href)
+    #     Args:
+    #         event: The click event.
+    #     """
+    #     if event.href.startswith("tag:"):
+    #         *_, tag = event.href.partition("tag:")
+    #         self.post_message(self.ShowTaggedWith(tag))
+    #     else:
+    #         open_url(event.href)
 
 
 ### details.py ends here
