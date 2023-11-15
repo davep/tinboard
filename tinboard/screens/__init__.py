8cb2ed146c26d13e8aa3a4c4d40ed9cb1470698f
Dave Pearson <davep@davep.org>
2023-11-15 06:43:07 +0000

    :bulb: Tidy up an init file.

--- tinboard/screens/__init__.py
+++ tinboard/screens/__init__.py
@@ -1,8 +1,12 @@
 """The screens for the application."""
 
+##############################################################################
+# Local imports.
 from .main import Main
 from .token_input import TokenInput
 
+##############################################################################
+# Exports.
 __all__ = ["Main", "TokenInput"]
 
 ### __init__.py ends here
