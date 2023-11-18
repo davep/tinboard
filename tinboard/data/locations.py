d4ccf453a64412aaab4660d4e9bfd12dc931515f
Dave Pearson <davep@davep.org>
2023-11-18 08:19:03 +0000

    :sparkles: Add support for locating a config directory

--- tinboard/data/locations.py
+++ tinboard/data/locations.py
@@ -6,7 +6,14 @@ from pathlib import Path
 
 ##############################################################################
 # XDG imports.
-from xdg_base_dirs import xdg_data_home
+from xdg_base_dirs import xdg_config_home, xdg_data_home
+
+
+##############################################################################
+def _tinboard_dir(root: Path) -> Path:
+    """Given a root, ensure and return the TinBoard directory within it."""
+    (save_to := root / "tinboard").mkdir(parents=True, exist_ok=True)
+    return save_to
 
 
 ##############################################################################
@@ -18,10 +25,23 @@ def data_dir() -> Path:
 
     Note:
         If the directory doesn't exist, it will be created as a side-effect
-        of calling this functon.
+        of calling this function.
     """
-    (save_to := xdg_data_home() / "tinboard").mkdir(parents=True, exist_ok=True)
-    return save_to
+    return _tinboard_dir(xdg_data_home())
+
+
+##############################################################################
+def config_dir() -> Path:
+    """The path to the configuration directory for the application.
+
+    Returns:
+        The path to the configuration directory for the application.
+
+    Note:
+        If the directory doesn't exist, it will be created as a side-effect
+        of calling this function.
+    """
+    return _tinboard_dir(xdg_config_home())
 
 
 ### locations.py ends here
