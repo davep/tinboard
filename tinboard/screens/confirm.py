f614187c701ab876f429af7ea65b7482e9dc512b
Dave Pearson <davep@davep.org>
2023-12-03 16:28:39 +0000

    :sparkles: Add a confirmation dialog

---
+++ tinboard/screens/confirm.py
@@ -0,0 +1,93 @@
+"""Provides a confirmation dialog."""
+
+##############################################################################
+# Textual imports.
+from textual import on
+from textual.app import ComposeResult
+from textual.containers import Horizontal, Vertical
+from textual.screen import ModalScreen
+from textual.widgets import Button, Label
+
+
+##############################################################################
+class Confirm(ModalScreen[bool]):
+    """A modal dialog for confirming things."""
+
+    CSS = """
+    Confirm {
+        align: center middle;
+    }
+
+    Confirm > Vertical {
+        padding: 1 2;
+        height: auto;
+        width: auto;
+        max-width: 80%;
+        background: $surface;
+        border: panel $error;
+        border-title-color: $text;
+    }
+
+    Confirm Label {
+        width: 1fr;
+        padding-left: 1;
+        padding-right: 1;
+        margin-bottom: 1;
+    }
+
+    Confirm > Vertical > Horizontal {
+        height: auto;
+        width: 100%;
+        align-horizontal: center;
+    }
+
+    Confirm Button {
+        margin-right: 1;
+    }
+    """
+
+    BINDINGS = [
+        ("escape", "no"),
+        ("f2", "yes"),
+        ("left", "focus_previous"),
+        ("right", "focus_next"),
+    ]
+
+    def __init__(
+        self, title: str, question: str, yes_text: str = "Yes", no_text: str = "No"
+    ) -> None:
+        """Initialise the dialog.
+
+        Args:
+            title: The title for the dialog.
+            question: The question to ask the user.
+            yes_text: The text for the yes button.
+            no_text: The text for the no button.
+        """
+        super().__init__()
+        self._title = title
+        self._question = question
+        self._yes = yes_text
+        self._no = no_text
+
+    def compose(self) -> ComposeResult:
+        """Compose the layout of the dialog."""
+        with Vertical() as dialog:
+            dialog.border_title = self._title
+            yield Label(self._question)
+            with Horizontal():
+                yield Button(f"{self._no} [dim]\\[Esc][/]", id="no")
+                yield Button(f"{self._yes} [dim]\\[F2][/]", id="yes")
+
+    @on(Button.Pressed, "#yes")
+    def action_yes(self) -> None:
+        """Send back the positive response."""
+        self.dismiss(True)
+
+    @on(Button.Pressed, "#no")
+    def action_no(self) -> None:
+        """Send back the negative response."""
+        self.dismiss(False)
+
+
+### confirm.py ends here
