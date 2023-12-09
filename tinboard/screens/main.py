bac95e281397309684edacfa6bcd6f726d767df4
Dave Pearson <davep@davep.org>
2023-12-09 09:04:15 +0000

    :sparkles: Add some more help for global keys and commands

--- tinboard/screens/main.py
+++ tinboard/screens/main.py
@@ -76,6 +76,13 @@ class Main(Screen[None]):
     | <kbd>Ctrl</kbd>+<kbd>l</kbd> | `Redownload/refresh bookmarks` | Reload the local bookmarks from Pinboard. |
     | <kbd>Ctrl</kbd>+<kbd>q</kbd> | `Quit the application` | Shockingly... quit the application! |
     | <kbd>Ctrl</kbd>+<kbd>p</kbd> | | Show the command palette. |
+    | <kbd>a</kbd> | `Show All` | Show all bookmarks. |
+    | <kbd>R</kbd> | `Show Unread` | Show all unread bookmarks. |
+    | <kbd>r</kbd> | `Show Read` | Show all read bookmarks. |
+    | <kbd>P</kbd> | `Show Private` | Show all private bookmarks. |
+    | <kbd>p</kbd> | `Show Public` | Show all public bookmarks. |
+    | <kbd>T</kbd> | `Show Untagged` | Show all untagged bookmarks. |
+    | <kbd>t</kbd> | `Show Tagged` | Show all tagged bookmarks. |
     """
 
     TITLE = "Tinboard"
