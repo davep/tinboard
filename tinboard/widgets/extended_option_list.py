6aa180c9bffa7be1d0517ac6c842056209df0f2a
Dave Pearson <davep@davep.org>
2023-12-12 13:17:32 +0000

    :truck: Move the OptionList workaround up a level

--- tinboard/widgets/extended_option_list.py
+++ tinboard/widgets/extended_option_list.py
@@ -1,6 +1,10 @@
 """A version of Textual's OptionList with some more navigation options."""
 
 ##############################################################################
+# Python imports.
+from typing_extensions import Self
+
+##############################################################################
 # Textual imports.
 from textual.binding import Binding
 from textual.widgets import OptionList
@@ -14,6 +18,12 @@ class OptionListEx(OptionList):
         Binding("s, j", "cursor_down", show=False),
         Binding("w, k", "cursor_up", show=False),
     ]
+
+    def clear_options(self) -> Self:
+        """Workaround for https://github.com/Textualize/textual/issues/3714"""
+        super().clear_options()
+        self._clear_content_tracking()
+        return self
 
 
 ### extended_option_list.py ends here
