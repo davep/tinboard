5b2ec9dc1d3e2fe01f1d6a3a6217383b85bd4449
Dave Pearson <davep@davep.org>
2024-04-05 16:21:41 +0100

    :bug: Ensure the initial filter is passed to the bounced screen

--- tinboard/app.py
+++ tinboard/app.py
@@ -53,7 +53,7 @@ class Tinboard(App[ExitStates]):
         """
         if token:
             token_file().write_text(token, encoding="utf-8")
-            self.push_screen(Main(API(token)))
+            self.push_screen(Main(API(token), self._initial_filter))
         else:
             self.exit(ExitStates.TOKEN_NEEDED)
 
