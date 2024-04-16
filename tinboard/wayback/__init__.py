96ccd34c0b4d602d4bb512ac443dfc2eef55d6e1
Dave Pearson <davep@davep.org>
2024-04-16 16:05:59 +0100

    :sparkles: Add an API for checking for wayback availability

---
+++ tinboard/wayback/__init__.py
@@ -0,0 +1,11 @@
+"""Code for walking to the Wayback machine."""
+
+##############################################################################
+# Local imports.
+from .api import Availability, availability
+
+##############################################################################
+# Export.
+__all__ = ["Availability", "availability"]
+
+### __init__.py ends here
