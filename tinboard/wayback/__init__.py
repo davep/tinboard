687e5fb93129b2bf9f4575412f7baaa3d53c7cad
Dave Pearson <davep@davep.org>
2024-04-16 16:09:41 +0100

    :sparkles: Export the Wayback error.

--- tinboard/wayback/__init__.py
+++ tinboard/wayback/__init__.py
@@ -2,10 +2,10 @@
 
 ##############################################################################
 # Local imports.
-from .api import Availability, availability
+from .api import Availability, WaybackError, availability
 
 ##############################################################################
 # Export.
-__all__ = ["Availability", "availability"]
+__all__ = ["Availability", "availability", "WaybackError"]
 
 ### __init__.py ends here
